ef391a139b6d3caab77584977addce090ca98bc2
jiayue zhu <1638790221@qq.com>
2018-08-07 19:25:44 +0800

    rich's safe version

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
     resources :comments, only: [ :index, :create]
   end
 
-  resources :users, shallow: true, only: [:show]
-
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
