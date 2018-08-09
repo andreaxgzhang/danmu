87e2656294876c89e80758b39c3b3c8cb839cfc8
jiayue zhu <1638790221@qq.com>
2018-08-09 11:51:35 +0800

    rich filter page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :slides, dependent: :destroy
   validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
-  # validates :username, uniqueness: true, length: { maximum: 20 }
+  validates :username, uniqueness: true, length: { maximum: 20 }
 end
