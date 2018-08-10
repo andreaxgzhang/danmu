dad1666bae0799256b45256656c3c68df4766e62
Dai Li <dai.thomas.li3@gmail.com>
2018-08-10 10:41:23 +0800

    show page and simple forms improvements

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -9,7 +9,7 @@ class SlidesController < ApplicationController
     @qr = RQRCode::QRCode.new("http://localhost:3000/slides/#{@slide.id}/comments/new")
     @svg = @qr.as_svg(offset: 0, color: '000',
                     shape_rendering: 'crispEdges',
-                    module_size: 11)
+                    module_size: 2.5)
 
   end
   def dashboard
@@ -24,6 +24,7 @@ class SlidesController < ApplicationController
   def create
     @slide = Slide.new(set_params)
     @slide.user = current_user
+    authorize @slide
     if @slide.save
       redirect_to @slide
     else
