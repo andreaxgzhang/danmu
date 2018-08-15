d6f42ea94fa12164bff7f7e31f88504c31cd5836
Xiange Zhang <xz4ee@virginia.edu>
2018-08-15 19:22:25 +0800

    rich not completed

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -3,10 +3,18 @@ class SlidesController < ApplicationController
   before_action :set_slide, only: [:show, :edit, :update, :destroy]
   def index
     @slides = policy_scope(Slide).order(created_at: :desc).select { |slide| slide.user == current_user}
+    @qrs = []
+    @slides.each do |slide|
+      @qr = RQRCode::QRCode.new("https://www.danmuonslide.com/slides/#{slide.id}/comments")
+      @qrs << @qr.as_svg(offset: 0, color: '000',
+                    shape_rendering: 'crispEdges',
+                    module_size: 5)
+    end
+
   end
   def show
     @slide = Slide.find(params[:id])
-    @qr = RQRCode::QRCode.new("https://danmu-on-slide.herokuapp.com/slides/#{@slide.id}/comments/new")
+    @qr = RQRCode::QRCode.new("https://www.danmuonslide.com/slides/#{@slide.id}/comments/new")
     @svg = @qr.as_svg(offset: 0, color: '000',
                     shape_rendering: 'crispEdges',
                     module_size: 5)
